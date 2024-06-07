265df0c48914834cd809f3b1fa4d510991f2033f
PrincessBeta <princessbranlobein@pm.me>
2024-06-07 22:17:37 +0200

    sample7 upgraded

--- samples/sample7.s
+++ samples/sample7.s
@@ -3,6 +3,7 @@
 
 	.equ	LED,	0xA0000000
 	.equ	TIM,	0xFF202000
+    .equ    DURATION, 0xFFFF
 
 _start:
 	ldr	R0, =LED
@@ -11,7 +12,7 @@ _start:
     add R2, R1, #0x8 // R2  = counter start low
     add R3, R1, #0x4 // R3  = control register
     mov R11, #1 //      R11 = bit flipped when timer reaches zero
-    mov R10, #125
+    ldr R10, =DURATION
     str R10, [R2]
     mov R10, #0b0110
     str R10, [R3]
