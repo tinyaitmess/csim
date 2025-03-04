08cfbd4b77994178489409a335a49bb0c0f58476
Iban Leginyora <ileginyora@DESKTOP-FD84G6H.>
2025-03-04 20:14:43 +0100

    Set <test_portd.s, test_portd.yaml, test_portd.elf> : Correction fichiers.

--- atmega328p/samples/test_portd.s
+++ atmega328p/samples/test_portd.s
@@ -1,16 +1,19 @@
-	.global _start
-	.global _exit
+    .global _start
+    .global _exit
 
-	.equ	LED,	0xA0000000
-	.equ	BUT,	0xB0000000
+    .equ LED,  0xA0000000 
+    .equ BUT,  0xB0000000 
+    .equ DDRD, 0x2A
+    .equ PORTD, 0x2B 
 
 _start:
-	ldr	R0, =LED
-	ldr	R1, =BUT
-loop:
-	ldr	R2, [R1]
-	str R2, [R0]
-	b	loop
+    ldr r0, =DDRD
+    mov r1, #0B001
+    strb r1, [r0]
+    ldr r0, =PORTD
+    mov r1, #0B001      
+    strb r1, [r0]
 
 _exit:
-	b	_exit
+    b _exit
+    
